548e0169c7cf4e9c79a97dd568c0deacf5151fb7
waifuZeroTwo <hoes.romeo@gmail.com>
2026-02-16 22:20:28 +0100

    Modernize packaging metadata with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-setup(name='stardew_fisher',
-      version='0.0.1',
-      install_requires=['pynput', 'gym', 'keras', 'tensorflow>=1.15.2']
-)
+
+if __name__ == "__main__":
+    setup()
